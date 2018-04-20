52dc90cff3d93e44ec942ba4217e91a801dd2b0c
Julia <julsuniverse@gmail.com>
2018-04-20 15:28:47 +0300

    parse currency

--- services/CurrencyService.php
+++ services/CurrencyService.php
@@ -4,14 +4,39 @@
 namespace app\services;
 
 
+use SimpleXMLElement;
+
 class CurrencyService
 {
     private $url = 'http://www.sedlabanki.is/xmltimeseries/Default.aspx?DagsFra=LATEST&GroupID=9&Type=xml';
 
+    const USD = 'USD.ISK.OVMI.S.D';
+    const EUR = 'EUR.ISK.OVMI.S.D';
+
     public function getXml()
     {
         $client = new \GuzzleHttp\Client();
-        $res = $client->request('GET', $this->url);
-        echo $res->getBody();
+        $xml = $client->request('GET', $this->url);
+
+        $xml = new SimpleXMLElement($xml->getBody());
+        $result = $this->getRate($xml);
+
+        print_r($result);
+    }
+
+    public function getRate($xml)
+    {
+        $result = [];
+
+        foreach ($xml as $currency) {
+            if ($currency->FameName == self::USD || $currency->FameName == self::EUR) {
+                $res['name'] = (string)$currency->FameName;
+                $res['date'] = (string)$currency->TimeSeriesData->Entry->Date;
+                $res['value'] = (string)$currency->TimeSeriesData->Entry->Value;
+
+                $result[] = $res;
+            }
+        }
+        return $result;
     }
 }
